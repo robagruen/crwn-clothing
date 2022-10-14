62b57830176cb3736eac539f31a513c60a557ac5
Rob Gruen <rob@robs-mbp.lan>
2022-10-14 15:49:47 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import './categories.styles.scss';
 
 function App() {
+
+  const categories = [
+    {
+      id: 0,
+      title: 'Hats',
+      subtitle: 'Shop Now'
+    },
+    {
+      id: 1,
+      title: 'Jackets',
+      subtitle: 'Shop Now'
+    },
+    {
+      id: 2,
+      title: 'Sneakers',
+      subtitle: 'Shop Now'
+    },
+    {
+      id: 3,
+      title: 'Women',
+      subtitle: 'Shop Now'
+    },
+    {
+      id: 4,
+      title: 'Men',
+      subtitle: 'Shop Now'
+    }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+      {
+        categories.map((category) => (
+          <div key={ category.id } className="category-container">
+            <div className="background-image">
+              {/* <img /> */}
+            </div>
+            <div className="category-body-container">
+              <h2>{ category.title }</h2>
+              <p>{ category.subtitle }</p>
+            </div>
+          </div>
+        ))
+      }  
     </div>
   );
 }
